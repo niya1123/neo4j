0ba9b3f6593d308319f8eca842858cc34e167e72
niya1123 <yoheikurioka@gmail.com>
2022-05-31 14:46:48 +0900

    python for neo4jのお試し

---
+++ python_dict/ex_finding_node.py
@@ -0,0 +1,20 @@
+from neo4j import GraphDatabase
+
+uri = "neo4j://neo4j:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "test"))
+
+def get_friends_of(tx, name):
+    friends = []
+    result = tx.run("MATCH (a:Person)-[:KNOWS]->(f) "
+                         "WHERE a.name = $name "
+                         "RETURN f.name AS friend", name=name)
+    for record in result:
+        friends.append(record["friend"])
+    return friends
+
+with driver.session() as session:
+    friends = session.read_transaction(get_friends_of, "Alice")
+    for friend in friends:
+        print(friend)
+
+driver.close()
---
+++ python_dict/example_create_node.py
@@ -0,0 +1,17 @@
+from neo4j import GraphDatabase
+
+uri = "neo4j://neo4j:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "test"))
+
+def create_friend_of(tx, name, friend):
+    tx.run("MATCH (a:Person) WHERE a.name = $name "
+           "CREATE (a)-[:KNOWS]->(:Person {name: $friend})",
+           name=name, friend=friend)
+
+with driver.session() as session:
+    session.write_transaction(create_friend_of, "Alice", "Bob")
+
+with driver.session() as session:
+    session.write_transaction(create_friend_of, "Alice", "Carl")
+
+driver.close()
