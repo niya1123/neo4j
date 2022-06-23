2077bee8365a85301aa740b8f5f8886224c864e7
niya1123 <yoheikurioka@gmail.com>
2022-06-23 11:37:11 +0900

    マージした

--- python_dict/webapi/grapgh_api.py
+++ python_dict/webapi/grapgh_api.py
@@ -21,9 +21,42 @@ def return_json(result):
     res = json.dumps(json_obj, ensure_ascii=False)
     return res
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def main():
     return render_template("main.html")
+
+@app.route('/', methods=["POST"])
+def main_post():
+    node1 = request.form["node1"]
+    node2 = request.form["node2"]
+    relationship = request.form["relationship"]
+    cnar = CNAR()
+    cnar.create_Node(node_name=node1)
+    cnar.create_Node(node_name=node2)
+    cnar.create_relationship(node1, node2, relationship)
+    cnar.close()
+    cn = CN()
+    session = cn.get_session()
+    result =session.run(CMQ._create_MATCH_query(node1_name=node1, node2_name=node2, relationship=relationship))
+    res = return_json(result)
+    cn.close()
+    return  jsonify(res)
+
+@app.route('/create/score', methods=["GET"])
+def create_score():
+    return render_template("score.html")
+
+@app.route('/create/score', methods=["POST"])
+def post_create_score():
+    node1 = request.form["node1"]
+    node2 = request.form["node2"]
+    score = request.form["score"]
+    cnar = CNAR()
+    cnar.create_Node(node_name=node1)
+    cnar.create_Node(node_name=node2)
+    cnar.create_score(node1, node2, score)
+    cnar.close()
+    return  "登録しました"
 
 @app.route('/get/all_graphs', methods=["GET"])
 def get_all_graph():
@@ -43,8 +76,25 @@ def post_node_and_relationship():
         print(request.headers['Content-Type'])
         return jsonify(res='error'), 400
 
-    print (request.json)
-
+    json_data = request.json
+    json_data_len = len(json_data)
+    type = json_data[0]['text']
+    parent_data = []
+    child_data = []
+    for i in range(json_data_len):
+        parent_id = json_data[i]['parent']
+        if parent_id == "type":
+           parent_data.append({'type': type, 'node_name': json_data[i]['text'], 'id': json_data[i]['id']})
+        elif parent_id == "parent":
+            child_data.append({'type': type, 'node_name': json_data[i]['text'], 'parent_id': "parent"})
+        else:
+            if parent_id in [parent['id'] for parent in parent_data]:
+                child_data.append({'type': type, 'node_name': json_data[i]['text'], 'parent_id': parent_id})
+    result = [parent['node_name'] + '-' + child['node_name'] for parent in parent_data for child in child_data if parent['id'] == child['parent_id']]
+    for r in result:
+        node1, node2 = tuple(filter(None, r.split('-')))
+        relationship = 'have'
+        create_node_and_relationship_with_type(node1, node2, relationship, type)
     return jsonify(res='ok')
 
 def create_node_and_relationship(node1, node2, relationship):
@@ -60,12 +110,18 @@ def create_node_and_relationship(node1, node2, relationship):
     cn.close()
     return  jsonify(res)
 
-# @app.route('/post/create_node_and_relationship', methods=["GET"])
-# def get_node_and_relationship():
-#     node1 = request.args.get("node1")
-#     node2 = request.args.get("node2")
-#     relationship = request.args.get("relationship")
-#     return render_template("result.html", message="{}と{}は{}という関係を持ちます. ".format(node1, node2, relationship))
+def create_node_and_relationship_with_type(node1, node2, relationship, type):
+    cnar = CNAR()
+    cnar.create_Node(node_name=node1, type=type)
+    cnar.create_Node(node_name=node2, type=type)
+    cnar.create_relationship(node1, node2, relationship)
+    cnar.close()
+    cn = CN()
+    session = cn.get_session()
+    result = session.run(CMQ._create_MATCH_query(node1_name=node1, node2_name=node2, relationship=relationship))
+    res = return_json(result)
+    cn.close()
+    return  jsonify(res)
 
 @app.route('/delete/<string:node_name>', methods=["GET"])
 def delete_node(node_name):
@@ -84,6 +140,20 @@ def delete_all_node():
 @app.route('/create/json', methods=["GET"])
 def create_json():
     return render_template("create_json.html")
+
+@app.route('/cytoscape', methods=["GET"])
+def cytoscape():
+    return render_template("cytoscape.html")
+
+@app.route('/get/score', methods=["GET"])
+def get_score():
+    cn = CN()
+    cmq = CMQ(cn.driver)
+    query = cmq._create_MATCH_score_query()
+    result = cn.get_session().run(query)
+    res = return_json(result)
+    cn.close()
+    return jsonify(res)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=7878, debug=True)
